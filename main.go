301246d1f7f43ac50368ce9d83aac25910017764
Andrei Boar <andrei.boar@movago.com>
2016-10-06 17:44:48 +0200

    improved db path to fetch from the same location

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/zuzuleinen/dave/database"
 	"github.com/zuzuleinen/dave/prompt"
+	"os"
+	"github.com/mitchellh/go-homedir"
 )
 
-const DB_PATH = "davedb.db"
+const DB_FILE = "davedb.db"
 
 func main() {
-	db := database.Connect(DB_PATH)
+	db := database.Connect(dbPath())
 	defer db.Close()
 
 	prompt.SayHello()
@@ -17,4 +19,8 @@ func main() {
 	prompt.ObeyCommand(command, db)
 }
 
+func dbPath() string {
+	homeDir, _ := homedir.Dir()
+	return homeDir + string(os.PathSeparator) + DB_FILE;
+}
 
