f3e36ce8e9c0aa355873ce0175b23e428ed966d4
Andrei Boar <andrei.boar@movago.com>
2016-10-11 12:20:19 +0200

    scan exact time

--- prompt/commands/remind_command.go
+++ prompt/commands/remind_command.go
@@ -12,6 +12,11 @@ import (
 	"time"
 )
 
+type ExactTime struct {
+	hours   int
+	minutes int
+}
+
 func Remind(db *sql.DB) {
 	fmt.Print("Task name: ")
 
@@ -23,11 +28,13 @@ func Remind(db *sql.DB) {
 	year := scanYear()
 	month := scanMonth()
 	day := scanDay()
-	hour := scanHour()
+	exactTime := scanExactTime()
+
+	fmt.Println(exactTime)
 
 	reminder.Save(db, dbItems)
 	color.Green("Great I will remind you to %s.", taskName)
-	fmt.Println("Time is", year, day, month, hour)
+	fmt.Println("Time is", year, day, month.String(), exactTime)
 }
 
 func scanYear() int {
@@ -46,30 +53,35 @@ func scanYear() int {
 	return y
 }
 
-func scanMonth() string {
+func scanMonth() time.Month {
 	var prompt string
-	var m string
+	var m time.Month
 
 	fmt.Print(fmt.Sprintf("Month[%d]:", time.Now().Month()))
 	fmt.Scanln(&prompt)
 
 	if prompt == "" {
-		m = time.Now().Month().String()
+		m = time.Now().Month()
 	} else {
 		monthIndex, _ := strconv.Atoi(prompt)
-		m = time.Month(monthIndex).String()
+		m = time.Month(monthIndex)
 	}
 
 	return m
 }
 
-func scanHour() string {
+func scanExactTime() ExactTime {
 	var prompt string
 
 	fmt.Print("Time(HH:mm):")
 	fmt.Scanln(&prompt)
 
-	return prompt
+	res := strings.Split(prompt, ":")
+
+	hours, _ := strconv.Atoi(res[0])
+	minutes, _ := strconv.Atoi(res[1])
+
+	return ExactTime{hours, minutes}
 }
 
 func scanDay() int {
