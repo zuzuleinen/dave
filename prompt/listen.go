7a5f872f5c138b3306c8dbbed0e308e8bf346c91
Andrei Boar <andrei.boar@movago.com>
2016-11-02 15:30:44 +0100

    small modiff

--- prompt/listen.go
+++ prompt/listen.go
@@ -26,12 +26,13 @@ func ObeyCommand(command string, db *sql.DB) {
 		commands.InstallDatabase(db)
 	case "remind":
 		commands.Remind(db)
+	case "reminders":
+		commands.ListReminders(db)
 	case "credential":
 		commands.Credential(db)
 	case "credentials":
 		commands.ListCredentials(db)
-	case "reminders":
-		commands.ListReminders(db)
+
 	default:
 		color.Red("Sorry, I dont understand '%s' command.", command)
 	}
