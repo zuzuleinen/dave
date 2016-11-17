fab66d43b30f8b8c8d1575194a5b321561f8c3d9
Andrei Boar <andrei.boar@movago.com>
2016-11-17 10:18:22 +0100

    finishup focus clear

--- config/mailgun.go
+++ config/mailgun.go
@@ -7,6 +7,10 @@ type MailGunConfig struct {
 }
 
 func Config() MailGunConfig {
-	config := MailGunConfig{"yourDomain", "privateKey", "publicKey"}
+	config := MailGunConfig{
+		"sandbox0071112e3f864716b83cd8ae6de50cd9.mailgun.org",
+		"key-da9d400c2fde42a612fdc6e0b4612017",
+		"pubkey-b272ce06375dc99cd552c08eec73f22f",
+	}
 	return config
 }
